0b59a175cdd83f7f9ca82ea45d67178faabd8831
EzequiasCodeMaster <gabrielezequias1524@gmail.com>
2024-11-21 12:22:20 -0300

    primeira parte completa

---
+++ script.js
@@ -0,0 +1,27 @@
+function enableHorizontalScroll(containerId) {
+  const container = document.getElementById(containerId);
+  container.addEventListener('mousedown', (e) => {
+      isDown = true;
+      startX = e.pageX - container.offsetLeft;
+      scrollLeft = container.scrollLeft;
+  });
+  container.addEventListener('mouseleave', () => {
+      isDown = false;
+  });
+  container.addEventListener('mouseup', () => {
+      isDown = false;
+  });
+  container.addEventListener('mousemove', (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - container.offsetLeft;
+      const walk = (x - startX) * 3; // Ajuste da sensibilidade do movimento
+      container.scrollLeft = scrollLeft - walk;
+  });
+}
+
+
+// Ativa a rolagem horizontal para cada contêiner de categoria
+enableHorizontalScroll('container-web');
+enableHorizontalScroll('container-mobile');
+enableHorizontalScroll('container-games');
